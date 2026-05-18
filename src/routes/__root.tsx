d35979d1b9d588b7f4b034c8b03772fba1b04a66
Nicole <npereira@confidencialimobiliario.com>
2026-05-18 12:53:39 +0100

    (routes): Head & Meta

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,28 +1,39 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
-
-import '../styles.css'
+import { HeadContent, Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+import { TanStackDevtools } from "@tanstack/react-devtools";
 
 export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        name: "description",
+        content:
+          "Share, explore and build on the best startup ideas and site hustles",
+      },
+      {
+        title: "IdeaDrop - Your Idea Hub",
+      },
+    ],
+  }),
   component: RootComponent,
-})
+});
 
 function RootComponent() {
   return (
     <>
+      <HeadContent />
       <Outlet />
       <TanStackDevtools
         config={{
-          position: 'bottom-right',
+          position: "bottom-right",
         }}
         plugins={[
           {
-            name: 'TanStack Router',
+            name: "TanStack Router",
             render: <TanStackRouterDevtoolsPanel />,
           },
         ]}
       />
     </>
-  )
+  );
 }
--- src/routes/ideas/index.tsx
+++ src/routes/ideas/index.tsx
@@ -1,6 +1,13 @@
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/ideas/")({
+  head: () => ({
+    meta: [
+      {
+        title: "IdeaDrop - Browse Ideas",
+      },
+    ],
+  }),
   component: IdeasPage,
 });
 
