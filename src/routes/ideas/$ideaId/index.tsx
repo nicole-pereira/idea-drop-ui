4b02642cdc4666055894108fd7287d6a85ffed88
Nicole <npereira@confidencialimobiliario.com>
2026-05-18 14:03:28 +0100

    (IdeaDetailsPage): Fetch data with TanStack Router Loaders

--- src/routes/ideas/$ideaId/index.tsx
+++ src/routes/ideas/$ideaId/index.tsx
@@ -1,9 +1,21 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+const fetchIdea = async (ideaId: string) => {
+  const res = await fetch(`http://localhost:8000/ideas/${ideaId}`);
+
+  if (!res.ok) throw new Error("Failed to fetch data");
+
+  return res.json();
+};
+
 export const Route = createFileRoute("/ideas/$ideaId/")({
   component: IdeaDetailsPage,
+  loader: async ({ params }) => {
+    return fetchIdea(params.ideaId);
+  },
 });
 
 function IdeaDetailsPage() {
-  return <div>Hello "/ideas/$ideaId/"!</div>;
+  const idea = Route.useLoaderData();
+  return <div>{idea.title}</div>;
 }
